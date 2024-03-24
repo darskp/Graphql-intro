58fffaa6b6b7b2fa4ef08c1d892caa76d08b681a
darskp <darshanpatgar100@gmail.com>
2024-03-25 00:34:13 +0530

    added css

--- client/src/component/TodoLists.jsx
+++ client/src/component/TodoLists.jsx
@@ -16,31 +16,30 @@ const TodoLists = () => {
     console.log(loading)
     console.log(error)
     console.log(data?.getTodos.length)
-    return (<div className="todo-container">
-        <h2>Todo List</h2>
-        {loading && <p>Loading...</p>}
-        {(error) && <p>Error : {error?.message}</p>}
+    return (
+        <div className="todo-container">
+            <h2>Todo List</h2>
+            {loading && <p>Loading...</p>}
+            {error && <p>Error: {error.message}</p>}
+            {data && data.getTodos.length > 0 ? (
+                <div className="todo-list">
+                    {data.getTodos.map((item, index) => (
+                        <div className="todo-item" key={index}>
+                            <h3>{item.title}</h3>
+                            <div className="user-info">
+                                <h4>User Information</h4>
+                                <p>Name: {item.user.name}</p>
+                                <p>Email: {item.user.email}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <p>No Data</p>
+            )}
+        </div>
 
-        {!error && data?.getTodos?.length > 0 ?
-            <div className="main-todo">
-                {data?.getTodos.map((item, index) => {
-                    return (<div className="inner-todo">
-                        <h3>
-                            Todo Title : {item?.title}
-                        </h3>
-                        <h4>User Information</h4>
-                        <p>
-                            Name :{item?.user?.name}
-                        </p>
-                        <p>
-                            Email :{item?.user?.email}
-                        </p>
-                    </div>)
-                })}
-            </div> :
-            <p>No Data</p>
-        }
-    </div>);
+   );
 }
 
 export default TodoLists;
